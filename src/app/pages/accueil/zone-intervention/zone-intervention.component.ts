17ebcf8922b752386ddaea9507c1583a1b7bae2b
Christopher Marie-Angélique <christopher.marieangelique.pro@gmail.com>
2024-05-26 21:16:10 +0200

    save

--- src/app/pages/accueil/zone-intervention/zone-intervention.component.ts
+++ src/app/pages/accueil/zone-intervention/zone-intervention.component.ts
@@ -6,24 +6,15 @@ import { Component } from '@angular/core';
   styleUrl: './zone-intervention.component.css'
 })
 export class ZoneInterventionComponent {
-  showWorldFrame: boolean = true;
-  showFranceFrame: boolean = false;
+  showFranceFrame: boolean = true;
   showBeninFrame: boolean = false;
 
-  showWorld() {
-    this.showWorldFrame = true;
-    this.showFranceFrame = false;
-    this.showBeninFrame = false;
-  }
-
   showFrance() {
-    this.showWorldFrame = false;
     this.showFranceFrame = true;
     this.showBeninFrame = false;
   }
 
   showBenin() {
-    this.showWorldFrame = false;
     this.showFranceFrame = false;
     this.showBeninFrame = true;
   }
